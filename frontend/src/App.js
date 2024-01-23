231d39975cd1a236d97c84aa0566006b1ab5e72c
keipy27 <kevinandris27@gmail.com>
2024-01-24 11:42:11 +1300

    Added routes for pages, components and pages folder also created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+
 function App() {
-  return <div className="App">Hi World!</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
