91dd3720e3f73dd08e620c52f40b11d17ff1bac0
keipy27 <kevinandris27@gmail.com>
2024-02-21 15:09:54 +1300

    Add and Display images function added

--- frontend/src/components/admin/productForm/UploadWidget.js
+++ frontend/src/components/admin/productForm/UploadWidget.js
@@ -1,10 +1,35 @@
 // ! CHILD class -- exported to `ProductForm.js`
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../card/Card";
 import { AiOutlineCloudUpload } from "react-icons/ai";
+import { BsTrash } from "react-icons/bs";
 
 const UploadWidget = () => {
-  const addImages = () => {};
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [images, setImages] = useState([]);
+  const [progress, setProgress] = useState(0);
+  const [uploading, setUploading] = useState(false);
+
+  // ! This function is passed on `input field` at `onChange` property.
+  const addImages = (e) => {
+    const selectedFiles = e.target.files;
+
+    /* >>> Fetched and stored all the files in this variable. */
+    const selectedFilesArray = Array.from(selectedFiles);
+
+    /* >>> Convert the selected images array into a URL. */
+    const imagesArray = selectedFilesArray.map((file) => {
+      return URL.createObjectURL(file);
+    });
+
+    /* >>> This would be sent to cloudinary. */
+    setImages((prevImages) => prevImages.concat(selectedFilesArray));
+
+    /* >>> Concat the images to the state -- this is used to display on the browser. */
+    setSelectedImages((prevImages) => prevImages.concat(imagesArray));
+
+    e.target.value = "";
+  };
 
   return (
     <div>
@@ -22,6 +47,22 @@ const UploadWidget = () => {
           />
         </label>
         <br />
+
+        {/* // ! View Selected Images */}
+        <div className={selectedImages.length > 0 ? "images" : ""}>
+          {selectedImages !== 0 &&
+            selectedImages.map((image, index) => {
+              return (
+                <div key={image} className="image">
+                  <img src={image} alt="productImage" width={200} />
+                  <button className="-btn">
+                    <BsTrash size={25} />
+                  </button>
+                  <p>{index + 1}</p>
+                </div>
+              );
+            })}
+        </div>
       </Card>
     </div>
   );
